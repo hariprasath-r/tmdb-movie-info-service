766801bfa5b6fbeb0264bfff5cf674f1485d994f
Hariprasath <balaprasad24@gmail.com>
2020-04-28 23:11:11 +0530

    Initial Commit

---
+++ src/test/java/in/hp/boot/movieinfoservice/MovieInfoServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package in.hp.boot.movieinfoservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieInfoServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

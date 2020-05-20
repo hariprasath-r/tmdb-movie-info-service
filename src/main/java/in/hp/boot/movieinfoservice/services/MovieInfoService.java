b1f3b1108d53e33996be369a9a3f8128703e413e
Hariprasath <balaprasad24@gmail.com>
2020-05-21 01:40:18 +0530

    Added @Slf4j loggers

--- src/main/java/in/hp/boot/movieinfoservice/services/MovieInfoService.java
+++ src/main/java/in/hp/boot/movieinfoservice/services/MovieInfoService.java
@@ -4,10 +4,12 @@ import in.hp.boot.movieinfoservice.dto.MovieDto;
 import in.hp.boot.movieinfoservice.mapper.MovieToMovieDtoMapper;
 import in.hp.boot.movieinfoservice.models.Movie;
 import in.hp.boot.movieinfoservice.utils.MovieInfoServiceUrlHelper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
+@Slf4j
 @Service
 public class MovieInfoService {
 
@@ -21,7 +23,9 @@ public class MovieInfoService {
     private MovieInfoServiceUrlHelper movieInfoServiceUrlHelper;
 
     public MovieDto getMovie(String id) {
+        log.info("getMovie movieId [{}]", id);
         Movie movie = restTemplate.getForObject(movieInfoServiceUrlHelper.getMovieUriId(id), Movie.class);
+        log.info("getMovie done movieId [{}]", id);
         return movieToMovieDtoMapper.toMovieDto(movie);
     }
 }
